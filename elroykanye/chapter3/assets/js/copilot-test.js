fb8c9017c74112c717043df96f3af3377fc9fe80
Elroy Kanye <elroykanye@gmail.com>
2021-07-13 15:34:56 +0100

    did first test on github copilot

---
+++ elroykanye/chapter3/assets/js/copilot-test.js
@@ -0,0 +1,24 @@
+function checkIfNumbersAreEqual(number1, number2) {
+    if (number1 === number2) {
+        return true;
+    }
+    return false;
+}
+
+//function to return greater number of two numbers
+function max(number1, number2) {
+    if (number1 > number2) {
+        return number1;
+    }
+    return number2;
+}
+    
+
+// Function to take two numbers
+// and return the greater number of the two 
+function greaterNumber(number1, number2) {
+    if (number1 > number2) {
+        return number1;
+    }
+    return number2;
+}
