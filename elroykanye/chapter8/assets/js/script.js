ff95c638944c396106d08c7cfe7cb6bd9df2edd7
Elroy Kanye <elroykanye@gmail.com>
2021-07-14 13:36:14 +0100

    added test function for changing bg colors on hover

--- elroykanye/chapter8/assets/js/script.js
+++ elroykanye/chapter8/assets/js/script.js
@@ -2,6 +2,8 @@ function displayHello () {
     let message = document.getElementById("message");
     message.outerHTML = "<h1>Hello World</h1>";
 }
+
+
 function generateEmail(form) {
     let firstName = form.elements["first"].value;
     let secondName = form.elements["second"].value;
@@ -11,4 +13,14 @@ function generateEmail(form) {
 
     form.reset();
     form.elements["first"].focus();
+}
+
+function makeBgRed() {
+    ele = document.getElementById("email-container");
+    ele.style="background-color: red; opacity: .75;";
+}
+
+function makeBgBlue() {
+    ele = document.getElementById("email-container");
+    ele.style="background-color: blue; opacity: .75;";
 }
