d6b55aa3296d542d8c60ef9f745889e9a7cd016c
Elroy Kanye <elroykanye@gmail.com>
2021-07-14 13:35:50 +0100

    added function for email generation

--- elroykanye/chapter8/assets/js/script.js
+++ elroykanye/chapter8/assets/js/script.js
@@ -2,3 +2,13 @@ function displayHello () {
     let message = document.getElementById("message");
     message.outerHTML = "<h1>Hello World</h1>";
 }
+function generateEmail(form) {
+    let firstName = form.elements["first"].value;
+    let secondName = form.elements["second"].value;
+    let email = firstName + "." + secondName + "@email.com";
+    emailPlace = document.getElementById("email");
+    emailPlace.innerHTML = email;
+
+    form.reset();
+    form.elements["first"].focus();
+}
