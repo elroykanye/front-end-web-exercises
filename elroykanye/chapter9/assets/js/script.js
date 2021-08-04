791fb82aed4d928f9556476ed6a21f94af856ac9
Elroy Kanye <elroykanye@gmail.com>
2021-08-04 22:39:36 +0100

    followed p2 ordering from book

--- elroykanye/chapter9/assets/js/script.js
+++ elroykanye/chapter9/assets/js/script.js
@@ -9,3 +9,19 @@ function gameNightPopUp() {
         message.innerHTML = "Whoa! Get a life yo...";
     }
 }
+
+function orderWords(form) {
+    var tb1 = form.elements["word1"];
+    var tb2 = form.elements["word2"];
+    var msg;
+    var word1 = tb1.value;
+    var word2 = tb2.value;
+
+    if (word1.toLowerCase() > word2.toLowerCase()) {
+        tb1.value = word2;
+        tb2.value = word1;
+        msg = "Congrats! The words are now in order";
+    } else {
+        msg = "The words are already in order";
+    } document.getElementById("message").innerHTML = msg;
+}  
